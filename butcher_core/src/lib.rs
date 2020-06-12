66c564500e26eaea92d4ab3fe33ae7e70d273810
Sasha <sasha.pourcelot@protonmail.com>
2020-06-12 22:19:06 +0200

    Add ButcherField trait in butcher_core
    This trait will be implemented for every field-structs, created by a derive macro.
    This will allow to handle multiple scenarios when butchering special fields.

--- butcher_core/src/lib.rs
+++ butcher_core/src/lib.rs
@@ -1,7 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use std::borrow::Cow;
+
+pub trait ButcherField: ToOwned {
+    type Output: ToOwned + Sized;
+
+    fn from_borrowed(b: &Self) -> Cow<Self::Output>;
+
+    fn from_owned(o: Self) -> Cow<'static, Self::Output>;
 }
