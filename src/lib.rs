8506f54b33e3772960bc113745e9a89971d0309d
Sasha <sasha.pourcelot@protonmail.com>
2020-06-12 21:53:56 +0200

    Add more documentation for various patterns and the butcher_struct temporary fix
    The "true" butcher_struct procedural macro will be avalaible when rust 1.45 will
    be released.

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,37 @@
 //! An easy way to interact with `Cow`ed structs and enums.
 //!
 //! This crate proposes some simple solutions to the most common patterns I met
-//! while working with `Cow`s in Rust. It currently fixes the iteration pattern.
-//! Other patterns will be added in a near future.
+//! while working with `Cow`s in Rust. It currently fixes two patterns:
+//! destructuring and iteration related to objects wrapped in `Cow`.
+//!
+//! ## Destructuring
+//!
+//! The [`butcher_struct`] macro allows to destructurate any struct. See the
+//! following example for a quick explanation of its usage.
+//!
+//! It should be noted that the type returned for some field may not be optimal.
+//! For example, a `Cow<String>` can be replaced by a `Cow<str>`. This is
+//! currently not supported by `butcher_struct`
+//!
+//! [`butcher_struct`]: macro.butcher_struct.html
+//!
+//! ```rust
+//! use std::borrow::Cow;
+//! use butcher::butcher_struct;
+//!
+//! #[derive(Clone)]
+//! struct Book {
+//!     title: String,
+//!     id: usize,
+//!     author: Box<str>,
+//!     // An useless field
+//!     is_opened: bool,
+//! }
+//!
+//! fn destructure_book(b: Cow<Book>) -> (Cow<usize>, Cow<String>, Cow<Box<str>>) {
+//!     butcher_struct!(b: Book, id, title, author)
+//! }
+//! ```
 //!
 //! ## Iteration
 //!
@@ -29,3 +58,47 @@
 //! [`CowIter`]: iterator/enum.CowIter.html
 
 pub mod iterator;
+
+// This is blocked by issue #54727. See it on github:
+// https://github.com/rust-lang/rust/issues/54727
+//
+// As such, we have to switch to a hand-made declarative macro.
+// pub use butcher_proc_macro::butcher_struct;
+
+#[macro_export]
+macro_rules! butcher_struct {
+    {
+        $var_name: ident : $ty: ident,
+        $( $field: ident ),+ $(,)?
+    } => {{
+        use std::borrow::Cow;
+        match $var_name {
+            Cow::Owned($ty {
+                $(
+                    $field,
+                )+
+                ..
+            }) => (
+                $(
+                    Cow::Owned($field),
+                )+
+            ),
+            Cow::Borrowed($ty {
+                $(
+                    $field,
+                )+
+                ..
+            }) => (
+                $(
+                    Cow::Borrowed($field),
+                )+
+            ),
+        }
+    }};
+
+    {
+        $var_name: ident : $ty: ident $( , )?
+    } => {
+        compile_error!("This non-distructuring is equivalent of a Drop. Use this function instead.");
+    }
+}
