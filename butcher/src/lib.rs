73bffdd69465bb96f16788b9419ed38a478834e2
Sasha <sasha.pourcelot@protonmail.com>
2020-07-19 15:02:42 +0200

    Removes butcher_core, so that there are only two crates in the workspace
    This way, publishing new version will be easier.
    Also, the 0.2.0 release has been tagged.

--- butcher/src/lib.rs
+++ butcher/src/lib.rs
@@ -59,5 +59,19 @@ pub mod iterator;
 
 pub use butcher_proc_macro::*;
 
-pub use butcher_core::Butcher;
-pub use butcher_core::ButcherField;
+use std::borrow::Cow;
+
+pub trait ButcherField<'a> {
+    type Input;
+    type Output: ToOwned + Sized + 'a;
+
+    fn from_borrowed(b: &'a Self::Input) -> Self::Output;
+
+    fn from_owned(o: Self::Input) -> Self::Output;
+}
+
+pub trait Butcher<'cow>: ToOwned + 'cow {
+    type Output: 'cow;
+
+    fn butcher(this: Cow<'cow, Self>) -> Self::Output;
+}
