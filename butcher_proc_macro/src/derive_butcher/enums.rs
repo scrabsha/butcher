262c6239e6b03c7adc7df783eb8129d2a0e4402d
Sasha <sasha.pourcelot@protonmail.com>
2020-07-21 12:54:28 +0200

    derive-butcher-enum: add all the serialization system
    Next commits will focus on bringing alive the expansion system.

--- butcher_proc_macro/src/derive_butcher/enums.rs
+++ butcher_proc_macro/src/derive_butcher/enums.rs
@@ -1,4 +1,9 @@
-use syn::{Data, DeriveInput, GenericParam, Ident, Variant as SVariant, Visibility, WhereClause};
+use std::collections::HashSet;
+
+use syn::{
+    punctuated::Punctuated, Data, DeriveInput, Fields, GenericParam, Ident, Lifetime,
+    Variant as SVariant, Visibility, WhereClause,
+};
 
 use proc_macro2::TokenStream;
 
@@ -27,9 +32,24 @@ impl ButcheredEnum {
             Data::Struct(_) | Data::Union(_) => unreachable!(),
         };
 
-        let variants = data.variants.into_iter().map(Variant::from).fold(
-            Ok(Vec::new()),
-            |acc, res| match (acc, res) {
+        let mut generic_types = HashSet::new();
+        let mut lifetimes = HashSet::new();
+
+        input.generics.params.into_iter().for_each(|g| match g {
+            GenericParam::Type(t) => {
+                generic_types.insert(t.ident);
+            }
+            GenericParam::Lifetime(lt) => {
+                lifetimes.insert(lt.lifetime);
+            }
+            GenericParam::Const(_) => {}
+        });
+
+        let variants = data
+            .variants
+            .into_iter()
+            .map(|v| Variant::from(v, &generic_types, &lifetimes))
+            .fold(Ok(Vec::new()), |acc, res| match (acc, res) {
                 (Ok(mut xs), Ok(x)) => {
                     xs.push(x);
                     Ok(xs)
@@ -40,8 +60,7 @@ impl ButcheredEnum {
                     Err(main_err)
                 }
                 (tmp @ Err(_), Ok(_)) => tmp,
-            },
-        )?;
+            })?;
 
         Ok(ButcheredEnum {
             name,
@@ -59,13 +78,55 @@ impl ButcheredEnum {
 
 struct Variant {
     name: Ident,
-    vis: Visibility,
-    kind: StructKind,
+    // TODO: discuss with syn author about visibility of variants
+    // Link in the references:
+    // https://doc.rust-lang.org/reference/items/enumerations.html
+    // vis: Visibility,
+    kind: VariantKind,
     fields: Vec<Field>,
 }
 
 impl Variant {
-    fn from(_v: SVariant) -> Result<Variant, syn::Error> {
-        todo!();
+    fn from(
+        v: SVariant,
+        generic_types: &HashSet<Ident>,
+        lifetimes: &HashSet<Lifetime>,
+    ) -> Result<Variant, syn::Error> {
+        let name = v.ident;
+
+        let (fields, kind) = match v.fields {
+            Fields::Named(fs) => (punctuated_to_vector(fs.named), VariantKind::Named),
+            Fields::Unnamed(fs) => (punctuated_to_vector(fs.unnamed), VariantKind::Unnamed),
+            Fields::Unit => (Vec::new(), VariantKind::Unit),
+        };
+
+        let fields = fields
+            .into_iter()
+            .enumerate()
+            .map(|(id, f)| Field::from(f, &generic_types, &lifetimes, id))
+            .fold(Ok(Vec::new()), |acc, res| match (acc, res) {
+                (Ok(mut main), Ok(v)) => {
+                    main.push(v);
+                    Ok(main)
+                }
+                (Ok(_), Err(e)) => Err(e),
+                (Err(mut main_err), Err(e)) => {
+                    main_err.combine(e);
+                    Err(main_err)
+                }
+                (tmp @ Err(_), Ok(_)) => tmp,
+            })?;
+
+        Ok(Variant { name, kind, fields })
     }
+}
+
+fn punctuated_to_vector<T, U>(punc: Punctuated<T, U>) -> Vec<T> {
+    punc.into_iter().collect()
+}
+
+enum VariantKind {
+    Named,
+    Unnamed,
+    Unit,
 }
