30d50c7ea0eb81191c4a41a1499a6e7c6b2b8691
Sasha <sasha.pourcelot@protonmail.com>
2020-07-29 11:58:50 +0200

    Add documentation for the FlattenCow trait

--- butcher/src/flatten.rs
+++ butcher/src/flatten.rs
@@ -6,7 +6,6 @@
 use std::borrow::Cow;
 use std::ops::Deref;
 
-// TODO: add doc
 fn flatten_cow<T>(input: Cow<T>) -> Cow<<T as Deref>::Target>
 where
     T: Deref + ToOwned,
@@ -19,7 +18,49 @@ where
     }
 }
 
-// TODO: add doc
+/// Allows to flatten some data wrapped in a `Cow`.
+///
+/// The best use case for this is to transform a `Cow<String>` into a
+/// `Cow<str>`. This also works for the boxed unsized type, as long as they
+/// implement the traits as required below.
+///
+/// # Example
+///
+/// The following example shows the flattening of `Cow<String>` and
+/// `Cow<PathBuf>`:
+///
+/// ```rust
+/// use std::{
+///     borrow::Cow,
+///     path::{Path, PathBuf},
+/// };
+///
+/// use butcher::flatten::FlattenCow;
+///
+/// // The type is annotated so that it is easier to understand what's happening
+/// let a: Cow<String> = Cow::Owned(String::from("Grace Hopper"));
+/// let a_flattened: Cow<str> = a.flatten();
+/// assert_eq!(a_flattened, "Grace Hopper");
+///
+/// let b: Cow<PathBuf> = Cow::Owned(PathBuf::from("/path/to/foo"));
+/// let b_flattened: Cow<Path> = b.flatten();
+/// assert_eq!(b_flattened, Path::new("/path/to/foo"));
+/// ```
+///
+/// # Traits requirements
+///
+/// In order to call `flatten` on a `Cow<T>`, the following requirements must be
+/// satisified:
+///   - `T` must implement [`ToOwned`], which is required to build the initial
+///   `Cow<T>`,
+///   - `T` must implement [`Deref`], and its `Target` must also implement
+///   [`ToOwned`], which is required to build the output
+///   `Cow<<T as Deref>::Target>`, and when the borrowed case is met,
+///   - `T` must be convertible into the `Owned` type associated to the `Target`
+///   dereferenced type, which is required when the owned case is met.
+///
+/// [`ToOwned`]: https://doc.rust-lang.org/std/borrow/trait.ToOwned.html
+/// [`Deref`]: https://doc.rust-lang.org/std/ops/trait.Deref.html
 pub trait FlattenCow<'cow, T>
 where
     T: Deref,
