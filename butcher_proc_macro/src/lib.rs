46cb659d397e5c7f24ab0ec9836d128040894ec1
Sasha <sasha.pourcelot@protonmail.com>
2020-07-29 22:56:50 +0200

    Deprecate the butcher_struct macro
    This macro was very usefull before, but it does not work since the prototype of
    ButcherField has changed [1].
    
    This commit completely remove it from the codebase.
    
    [1]: https://github.com/scileo/butcher/pull/11

--- butcher_proc_macro/src/lib.rs
+++ butcher_proc_macro/src/lib.rs
@@ -1,29 +1,12 @@
 extern crate proc_macro;
 
-use crate::butcher_struct::ButcheredStruct;
-
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
 
 #[macro_use]
 mod utils;
 
-mod butcher_struct;
 mod derive_butcher;
-
-/// Allow simple destructuring.
-///
-/// This macro has the same utility as `MyStruct::butcher`, but allows to
-/// destructure only specific fields, and drop the others.
-#[proc_macro]
-pub fn butcher_struct(tokens: TokenStream) -> TokenStream {
-    let data = parse_macro_input!(tokens as ButcheredStruct);
-
-    // TODO: check that we are actually destructuring something
-    // TODO: check that there is no repetition
-
-    data.expand_to_code().into()
-}
 
 /// Derives the `Butcher` trait for a structure or an enum.
 ///
